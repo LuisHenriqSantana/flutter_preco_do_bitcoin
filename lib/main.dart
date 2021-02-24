9593d99334aa021d3cab50952cc432a214b1a6a2
Luis <luis.henrique8000@gmail.com>
2021-02-24 19:09:49 -0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:preco_do_bitcoin/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  )
+  );
+}
